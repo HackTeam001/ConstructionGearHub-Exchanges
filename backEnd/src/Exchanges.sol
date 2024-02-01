4ec44bf9647590f005a95ef14e21e754aa25e5fc
issa99-lab <issanana99@gmail.com>
2024-02-01 14:05:17 +0300

    Installed Openzeppelin lib: ReentrancyGual.sol

--- backEnd/src/Exchanges.sol
+++ backEnd/src/Exchanges.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
 contract Shop {
     //----Errors----//
