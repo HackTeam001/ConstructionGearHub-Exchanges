e1896d51a8fe2fe503f03e939690b0b54f45de9c
issa99-lab <issanana99@gmail.com>
2024-02-01 14:42:07 +0300

    Added closeShop and hire functions

--- backEnd/src/Exchanges.sol
+++ backEnd/src/Exchanges.sol
@@ -3,26 +3,28 @@ pragma solidity ^0.8.13;
 
 import "lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-contract Shop {
+contract Shop is ReentrancyGuard{
     //----Errors----//
     error notOwner();
 
     error itemNotListed();
 
     //----Events----//
-    event itemAdded(uint indexed id, string name, string description, uint price);
+    event ownerOpenedShop();
+    event ownerClosedShop();
 
+    event itemAdded(uint indexed id, string name, string description, uint price);
     event itemDeleted(uint indexed id);
 
     event itemUnlisted(uint indexed id);
 
-
-    address public owner;
-    bool public isOpen;
+    //Storage variables
+    address public s_owner;
+    bool public s_isOpen;
 
     constructor(){
-        owner = msg.sender;
-        isOpen = true;
+        s_owner = msg.sender;
+        s_isOpen = true;
     }
 
     struct Item{
@@ -38,7 +40,7 @@ contract Shop {
 
     /*@dev Only the owner has access to the function */
     modifier onlyOwner(){
-        if(msg.sender != owner){
+        if(msg.sender != s_owner){
             revert notOwner();
         }
         _;
@@ -53,35 +55,49 @@ contract Shop {
     }
 
     /*@dev button opens shop*/
-    function openShop() private onlyOwner{
-        isOpen = true;
+    function openShop() private onlyOwner nonReentrant{
+        s_isOpen = true;
+        emit ownerOpenedShop();
+    }
+
+    /*@dev button opens shop*/
+    function closeShop()  private onlyOwner nonReentrant{
+        s_isOpen = false;
+        emit ownerClosedShop();
     }
 
     /*@dev button adds item to shop*/
-    function addItem(uint _id, string memory _name, string memory _description, uint256 _price) private onlyOwner{
+    function addItem(uint _id, string memory _name, string memory _description, uint256 _price) private onlyOwner nonReentrant{
         items[_id] = Item(_name,_description,_price, true);
         emit itemAdded(_id, _name,_description,_price);
     }
 
     /*@dev button deletes item from shop*/
-    function deleteItem(uint _id) private onlyOwner{
+    function deleteItem(uint _id) private onlyOwner nonReentrant{
         delete items[_id];
         emit itemDeleted(_id);
     }
 
      /*@dev button unlists item from shop until it becomes available*/
-     function unlistItem(uint _id) private onlyOwner{
+     function unlistItem(uint _id) private onlyOwner nonReentrant {
         items[_id].listed = false;
         emit itemUnlisted(_id);
      }
 
-       /*@dev Users use this button to buy an item at the shop*/
+    /*@dev Users use this button to buy an item from the shop*/
      function buyItem(uint _id) external payable isListed(_id){
-        require(isOpen, "Shop is not open");
+        require(s_isOpen, "Shop is not open");
         require(msg.value >= items[_id].price, "Insufficient funds");
-        require(msg.sender != owner, "Shop owner cannot buy their own item");
+        require(msg.sender != s_owner, "Shop owner cannot buy their own item");
 
         //transfer happens via escrow contract (WILL MAKE THIS!!)
+     }
+
+    /*@dev Users use this button to buy an item from the shop*/
+     function hireItem(uint _id) external view isListed(_id){
+         require(s_isOpen, "Shop is not open");
+
+        //perform hiring via escrow contract
      }
 
 
