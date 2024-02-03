c82027559a79bcda6d84105ef80b9dee5ea00ce4
Peter Kagwe <kagwepeter07@gmail.com>
2024-02-03 04:13:14 +0300

    add skeleton

---
+++ frontend/src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Gears, Header, } from '../../components'
+
+
+const Home = () => {
+
+  return <div>
+   <Header />
+   <Gears title="Top Items"  />
+  </div>;
+};
+
+export default Home;
